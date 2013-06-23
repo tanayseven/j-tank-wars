3fb4311b7ea4d09c9b80f9ed4f14efc6f02a2185
tanayseven <tanayseven@gmail.com>
2013-06-23 10:13:34 +0530

    Removed the throws declaration for FileNotFoundException

--- src/Sound.java
+++ src/Sound.java
@@ -15,20 +15,20 @@ public class Sound
 	InputStream inEnemy;
 	AudioStream asEnemy;
 	
-	Sound() throws IOException, FileNotFoundException
+	Sound() throws IOException
 	{
 		inPlayer = new FileInputStream("sounds/gunshot2.wav");
 		asPlayer = new AudioStream(inPlayer);
 		
 		inEnemy = new FileInputStream("sounds/gunshot1.wav");
-		asEnemy = new AudioStream(inEnemy);;
+		asEnemy = new AudioStream(inEnemy);
 	}
 	
-	public void gunshotPlayer() throws IOException, FileNotFoundException
+	public void gunshotPlayer() throws IOException
 	{
 		AudioPlayer.player.start(asPlayer);
 	}
-	public void gunshotEnemy() throws IOException, FileNotFoundException
+	public void gunshotEnemy() throws IOException
 	{
 		AudioPlayer.player.start(asEnemy);
 	}
